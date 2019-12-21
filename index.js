d806f83c0055fdc01b82a107d58e43b9b488b16e
Ivan_Kalachyev <Ivan_Kalachyev@epam.com>
2019-12-21 14:59:43 +0300

    correcting typos

--- index.js
+++ index.js
@@ -4,15 +4,11 @@ const newman = require("newman");
 
 // All ready, lets start up the server
 http.createServer(function (req, res) {
-    handler(req, res, function (err) {
-        // handles unknown message types
-
-
         // Newman will run the tests in our postman collection
         // found at the specified URL. So that the tests can
         // run against any URL, we pass in the URL we worked
         // out above as a global variable
-        newman.run({
+    newman.run({
             collection: process.env.TESTS_COLLECTION_URL,
             globals: {  },
             reporters: "cli" // for now, just write the test output to the logs
@@ -31,7 +27,7 @@ http.createServer(function (req, res) {
                 console.log("tests completed with no failures");
                 //createStatus(event.payload, "success", "Tests completed with no failures");
             }
-        });
+   
 
         res.statusCode = 404;
         res.end('no such location');
